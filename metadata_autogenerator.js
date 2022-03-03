f737bc1537349de8ac5432fbd10c4abd4554ab74
Svenulf <61520316+Svenulf@users.noreply.github.com>
2022-03-03 16:05:26 +0100

    Update metadata_autogenerator.js

--- metadata_autogenerator.js
+++ metadata_autogenerator.js
@@ -7,24 +7,43 @@ import { meta_autogen, randomNum }  from './scripts/meta_autogen.mjs';
  * To run metadata_autogenerator: $ npm start or (ctrl + alt + n or F1 and "run code")
  */
 
-//-----------------------------------------------------------------------------------#
+//---------------------------------------------------------------#
 const startId = 1;
 const amount = 5;
-const metadataSource = "001001"; // input "randomNumber" if random metadata-schemes
+const metadataSource = "001001";
 // Random metadata
 const random = true; // toggle random on/off
-const maxRandomValue = 3;
-//-----------------------------------------------------------------------------------#
+const metadataTypes = 3;
+//---------------------------------------------------------------#
+
+const map = new Map();
 
 if (random) {
     for (let index = 1; index <= amount; index++) {
-        let randomMetadata = "00100" + String(randomNum(maxRandomValue));
+        let randomMetadata = "00100" + String(randomNum(metadataTypes));
         await meta_autogen(index, 1, randomMetadata).then((value) => 
             console.log("index: " + index + " with metadata: " + randomMetadata)
         )
+        // Log of random metadata distrubution
+        console.log("map value:",map.get(randomMetadata));
+        if (map.get(randomMetadata) > 0) {
+            let n = map.get(randomMetadata) + 1;
+            map.set(randomMetadata, n)
+        } else {
+            map.set(randomMetadata, 1)
+        }
     }
 } else {
     await meta_autogen(startId, amount, metadataSource).then((value) => 
         console.log(value + " Generated metadata files with id:  " + startId + " - " + (startId + amount - 1) + "  (" + amount + "x)")
     )
 };
+
+// Results
+console.log("\nAutogenerator finished! Files -> generated_batch folder");
+if (random) {
+    console.log("\nRandom metadata distrubution:");
+    map.forEach(function(value, key) {
+        console.log(key + ' = ' + value)
+      })
+}
